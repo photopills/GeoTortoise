eb3e98c52b30f019540f3b56e04216c552c254e1
Diogo <somnium@riseup.net>
2021-12-17 13:54:49 +0000

    Validate latitude (geom.y)

--- geotortoise/utils.py
+++ geotortoise/utils.py
@@ -1,2 +1,5 @@
 def longitude_is_valid(x: float) -> bool:
     return -180 <= x <= 180 
+
+def latitude_is_valid(y: float) -> bool:
+    return -90 <= y <= 90
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,7 +1,7 @@
 import pytest
 from shapely.geometry import Point
 
-from geotortoise.utils import longitude_is_valid
+from geotortoise.utils import longitude_is_valid, latitude_is_valid
 
 
 @pytest.mark.parametrize(
@@ -13,5 +13,14 @@ from geotortoise.utils import longitude_is_valid
         (Point(180, 0), True),
         (Point(0, 0), True),
         
+@pytest.mark.parametrize(
+    "geom, expected_result",
+    [
+        (Point(0, 91.01), False),
+        (Point(0, -90.01), False),
+        (Point(0, 42), True),
+        (Point(0, -42), True),
     ],
 )
+def test_latitude_is_valid(geom, expected_result):
+    assert latitude_is_valid(geom.y) is expected_result
